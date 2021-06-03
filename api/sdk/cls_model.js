1cd5226c7be24039394eab1acabb8f0fb372eeba
asepilham <84943488+asepilham@users.noreply.github.com>
2021-06-04 01:13:54 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -39,7 +39,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/jst_service/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/asepilham/asepilham_forum13_jst/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
